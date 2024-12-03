7b795067f841e27173c0147d1a7894d1aed6396e
Samuel Chuang <sam3855043@gmail.com>
2024-12-03 22:43:14 +0800

    read me add content

---
+++ quick-start/build.bat
@@ -0,0 +1,9 @@
+echo "Compiling : src/quick-start/price.per"
+fglform -M price.per
+echo "Compiling : src/quick-start/edit_price.per"
+fglform -M edit_price.per
+echo "Compiling : src/quick-start/price.4gl"
+fglcomp -M price.4gl
+
+echo "Generating quick-start guide Genero archive"
+fglgar gar -o ggc-quick-start.gar
